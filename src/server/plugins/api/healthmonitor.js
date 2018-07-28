dda894097d9547ee8077759c4e099be3b039236f
Кузьмин Андрей Борисович <ABKuzmin@alfabank.ru>
2018-07-28 07:28:36 +0300

    feat(api, health): - /health возвращает название и версию прилоежния

--- src/server/plugins/api/healthmonitor.js
+++ src/server/plugins/api/healthmonitor.js
@@ -4,7 +4,7 @@ import apiPluginFactory from '../../utils/api-plugin-factory';
 
 export default apiPluginFactory(
   config.common.apiConfig.health,
-  (data, request, reply) => reply('ok'),
+  (data, request, reply) => reply(`${config.common.appId}@${config.common.appVersion}`),
   {
     routeConfig: {
       // для этого обработчика авторизация не нужна
