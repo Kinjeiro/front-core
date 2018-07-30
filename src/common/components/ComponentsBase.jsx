8c1bafbbe33704ec699a1304b6513940bcd81d1b
Кузьмин Андрей Борисович <ABKuzmin@alfabank.ru>
2018-07-30 11:47:28 +0300

    feat(components): - добавил ComponentsBase - мапу всех компонентов, в которой есть возможность заменить компоненты в дочерних проектах (к примеру, стили Form, Loading, Button) + чтобы не грузились лишние css стили сделана поздняя инициализация через require

---
+++ src/common/components/ComponentsBase.jsx
@@ -0,0 +1,83 @@
+/* eslint-disable global-require */
+import React from 'react';
+
+import { executeVariableMemoize } from '../utils/common';
+
+const COMPONENTS_BASE = {
+  replaceComponent(name, ComponentClass) {
+    Object.defineProperty(this, name, {
+      enumerable: true,
+      configurable: true,
+      writable: false,
+      // value: ComponentClass,
+      get() {
+        // отложенная загрузка компонентов () => require('./Component);
+        return executeVariableMemoize(name, ComponentClass);
+      },
+    });
+    return this;
+  },
+
+
+  // ======================================================
+  // FORM UI
+  // ======================================================
+  get Form() {
+    return require('./form/CoreForm').default;
+  },
+  get FormLayout() {
+    return require('./form/FormLayout').default;
+  },
+  get Field() {
+    return require('./form/CoreField').default;
+  },
+  get FieldLayout() {
+    return require('./form/FieldLayout').default;
+  },
+
+  // ======================================================
+  // FORM FIELDS
+  // ======================================================
+  BaseInput: (props) => <input { ...props } />,
+  BaseTextArea: (props) => <textarea { ...props } />,
+  BaseNumberInput: (props) => <input { ...props } type="number" />,
+  get Input() {
+    return require('./form/fields/CoreInput').default;
+  },
+
+  BaseSelect: (props) => <select { ...props } />,
+  BaseOption: (props) => <option { ...props } />,
+  get Select() {
+    return require('./form/fields/CoreSelect').default;
+  },
+
+  DatePicker: () => null,
+  Checkbox: (props) => <input { ...props } type="checkbox" />,
+
+  // ======================================================
+  // UI
+  // ======================================================
+  get Loading() {
+    return require('./Loading/Loading').default;
+  },
+  get Button() {
+    return (props) => (<button { ...props } />);
+  },
+  get Link() {
+    return require('./Link/Link').default;
+  },
+  get ActionStatus() {
+    return require('./ActionStatus/ActionStatus').default;
+  },
+  get Notifications() {
+    return require('./Notifications/Notifications').default;
+  },
+  get Notice() {
+    return require('./Notifications/Notice').default;
+  },
+  get Modal() {
+    return null;
+  },
+};
+
+export default COMPONENTS_BASE;
