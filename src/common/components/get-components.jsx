5744d332f6a188cfd1a603840e64fec264908e6a
Кузьмин Андрей Борисович <ABKuzmin@alfabank.ru>
2018-08-09 19:34:50 +0300

    feat(cb): - обновил компонентный корные

--- src/common/components/get-components.jsx
+++ src/common/components/get-components.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 let CB = null;
 
 export function initComponents(COMPONENTS_BASE) {
@@ -20,25 +22,33 @@ export function initComponents(COMPONENTS_BASE) {
   COMPONENTS_BASE.replace('BaseTextArea', () => ({ controlRef, touched, ...props }) =>
     <textarea ref={ controlRef } { ...props } />);
   COMPONENTS_BASE.replace('TextArea', () => require('./form/fields/CoreTextArea').default);
-  COMPONENTS_BASE.replace('BaseSelect', () => (props) =>
-    <select { ...props } />);
+  COMPONENTS_BASE.replace('BaseSelect', () => ({ options, value }) =>
+    <select value={ value } />);
   COMPONENTS_BASE.replace('Select', () => require('./form/fields/CoreSelect').default);
-  COMPONENTS_BASE.replace('DatePicker', () => ({ controlRef, touched, ...props }) =>
-    <input ref={ controlRef } { ...props } />);
+  COMPONENTS_BASE.replace('DatePicker', () => ({ controlRef, value }) =>
+    <input ref={ controlRef } value={ value } type="datetime" />);
   COMPONENTS_BASE.replace('Checkbox', () => ({ controlRef, touched, ...props }) =>
     <input ref={ controlRef } { ...props } type="checkbox" />);
 
   // ======================================================
   // UI
   // ======================================================
-  COMPONENTS_BASE.replace('Loading', () => require('./Loading/Loading').default);
-  COMPONENTS_BASE.replace('Button', () => (props) => (<button { ...props } />));
-  COMPONENTS_BASE.replace('Link', () => require('./Link/Link').default);
   COMPONENTS_BASE.replace('ActionStatus', () => require('./ActionStatus/ActionStatus').default);
+  COMPONENTS_BASE.replace('Modal', () => require('./CoreModal/CoreModal').default);
+  COMPONENTS_BASE.replace('Link', () => require('./Link/Link').default);
+  COMPONENTS_BASE.replace('ListItem', () => require('./ListItem/ListItem').default);
+  COMPONENTS_BASE.replace('Loading', () => require('./Loading/Loading').default);
+  COMPONENTS_BASE.replace('MediaQuery', () => require('./MediaQuery/MediaQuery').default);
   COMPONENTS_BASE.replace('Notifications', () => require('./Notifications/Notifications').default);
   COMPONENTS_BASE.replace('Notice', () => require('./Notifications/Notice').default);
-  COMPONENTS_BASE.replace('Modal', () => require('./CoreModal/CoreModal').default);
-  COMPONENTS_BASE.replace('Segment', () => ({ children }) => (<div>{ children }</div>));
+  COMPONENTS_BASE.replace('ReadMore', () => require('./ReadMore/ReadMore').default);
+  COMPONENTS_BASE.replace('UniError', () => require('./UniError/UniError').default);
+
+  // ======================================================
+  // FUTURE UI
+  // ======================================================
+  COMPONENTS_BASE.replace('Button', () => (props) => (<button { ...props } />));
+  COMPONENTS_BASE.replace('Segment', () => ({ children, className }) => (<div className={ className }>{ children }</div>));
 
   // ======================================================
   // PAGES
