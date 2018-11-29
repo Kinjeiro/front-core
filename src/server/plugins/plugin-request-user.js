0eb0dd7d74ffd0b6ee427d1aecae9e4f8a78ff0a
Andrey Kuzmin <kinjeiro@gmail.com>
2018-11-29 16:03:04 +0300

    feat(server, request): - добавил к request isAdmin проперть, для удобства

--- src/server/plugins/plugin-request-user.js
+++ src/server/plugins/plugin-request-user.js
@@ -1,14 +1,19 @@
 /* eslint-disable no-param-reassign */
+import serverConfig from '../server-config';
+
 import { getCredentialsFromRequest } from '../utils/credentials-utils';
 import { getToken } from '../utils/auth-utils';
 
 export const REQUEST_FIELD__USER = 'user';
 export const REQUEST_FIELD__USER_TOKEN = 'userToken';
+export const REQUEST_FIELD__IS_ADMIN = 'isAdmin';
 
 function pluginRequestUser(server, options, next) {
   // после авторизации
   server.ext('onPostAuth', (request, reply) => {
-    request[REQUEST_FIELD__USER] = getCredentialsFromRequest(request).userInfo;
+    const { userInfo } = getCredentialsFromRequest(request);
+    request[REQUEST_FIELD__USER] = userInfo;
+    request[REQUEST_FIELD__IS_ADMIN] = userInfo && userInfo.roles.includes(serverConfig.common.features.auth.adminRoleName);
     request[REQUEST_FIELD__USER_TOKEN] = getToken(request);
     return reply.continue();
   });
