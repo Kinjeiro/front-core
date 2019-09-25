17456d4c594e883bdd2c41f45841235256174f57
Andrey Kuzmin <kinjeiro@gmail.com>
2019-09-25 10:56:52 +0300

    feat(form, PhoneInput): - убрал pattern ибо там используется маска, но тут большой вопрос валидации

--- src/modules/feature-ui-form/common/subModule/components/fields/PhoneInput/PhoneInput.jsx
+++ src/modules/feature-ui-form/common/subModule/components/fields/PhoneInput/PhoneInput.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import InputMask from 'react-input-mask';
 
+// todo @ANKU @LOW - разбить на отображение и логику
 export default class PhoneInput extends React.Component {
+  // todo @ANKU @CRIT @MAIN -
+  // static validate(value, fieldProps, domRef, getFormData) {
+  //   return [];
+  // }
+
   render() {
+    const {
+      pattern, // - убираем, так как не подходит - нужна маска
+      ...other
+    } = this.props;
     // todo @ANKU @LOW - адаптировать
     return (
       <InputMask
         mask="+7-999-999-99-99"
         permanents={ [0, 1, 2, 6, 9] }
-        { ...this.props }
+        { ...other }
       />
     );
   }
